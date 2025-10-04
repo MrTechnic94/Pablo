6d68f99d508d9a8e67c1fd7880d906129a2b978d
MrTechnic <bezimienny2@protonmail.com>
2025-10-04 13:01:24 +0200

    Dodanie emotek ktore sa potrzebne do dzialania bota

--- index.js
+++ index.js
@@ -12,6 +12,13 @@
  * [i] Niektore pliki (np. embedUpdater itp.) pobieraja zmienne
  * z configu co 2h jezeli ustawisz je recznie w 'default.json'.
  * Wiec mozna je zmieniac bez koniecznosci restartu bota.
+ * [i] Profilowe bota oraz emotki ktore sa wykorzystane do poprawnego
+ * dzialania bota sa dostepne w 'assets/profile' oraz 'assets/emoji'.
+ * Zalecane jest dodanie tych emotek dla bota w jego panelu
+ * (https://discord.com/developers/applications) oraz zmiane ID
+ * w pliku konfiguracyjnym 'default.json' w sekcji 'emojis'.
+ * UWAGA: emotki te nie sa moja wlasnoscia, zostaly skopiowane/pobrane
+ * z innych serwerow Discorda lub ze strony 'https://emoji.gg'.
  */
 
 'use strict';
