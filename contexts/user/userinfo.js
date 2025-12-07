01be6e425bc60ac1c9a6a65d69b75efedbd597b2
MrTechnic <bezimienny2@protonmail.com>
2025-12-07 18:10:06 +0100

    Aktualizacja userinfo

--- contexts/user/userinfo.js
+++ contexts/user/userinfo.js
@@ -24,16 +24,61 @@ module.exports = {
         const createdAt = Math.floor(targetMember.user.createdTimestamp / 1000);
         const joinedAt = Math.floor(targetMember.joinedTimestamp / 1000);
 
+        // Zaproszenia
+        let inviteCount = 0;
+
+        const invites = await interaction.guild.invites.fetch();
+        inviteCount = invites
+            .filter(i => i.inviter && i.inviter.id === targetMember.id)
+            .reduce((acc, invite) => acc + invite.uses, 0);
+
+        // Urzadzenie
+        const fullDeviceMap = {
+            desktop: { text: 'Komputer', emoji: '🖥️' },
+            mobile: { text: 'Telefon', emoji: '📱' },
+            web: { text: 'Przeglądarka', emoji: '🌐' },
+        };
+
+        const clientStatus = targetMember.presence?.clientStatus;
+
+        const devices = clientStatus
+            ? Object.keys(clientStatus).map(key => fullDeviceMap[key]?.text)
+            : [];
+
+        const deviceString = devices.join(', ') || 'Użytkownik jest offline.';
+
+        const deviceEmoji =
+            clientStatus?.desktop ? '🖥️' :
+                clientStatus?.mobile ? '📱' :
+                    clientStatus?.web ? '🌐' :
+                        '❓';
+
+        // Status
+        const fullStatusMap = {
+            online: { text: 'Dostępny', emoji: '🟢' },
+            idle: { text: 'Zaraz wracam', emoji: '🌙' },
+            dnd: { text: 'Nie przeszkadzać', emoji: '⛔' },
+            invisible: { text: 'Niedostępny', emoji: '🎱' },
+            offline: { text: 'Offline', emoji: '🎱' },
+        };
+
+        const statusKey = targetMember.presence?.status || 'Niedostępny';
+
+        const userStatus = fullStatusMap[statusKey]?.text || 'Niedostępny';
+        const statusEmoji = fullStatusMap[statusKey]?.emoji || '🎱';
+
         const successEmbed = createEmbed({
             title: 'Podgląd użytkownika',
             thumbnail: targetMember.user.displayAvatarURL(),
             fields: [
                 { name: '`👤` Użytkownik', value: `<@${targetMember.id}>` },
                 { name: '`✏️` Pseudonim', value: targetMember.nickname || 'Nie ustawiono' },
+                { name: `\`${deviceEmoji}\` Urządzenie`, value: deviceString },
+                { name: `\`${statusEmoji}\` Status`, value: userStatus },
                 { name: '`🚪` Dołączył na serwer', value: `<t:${joinedAt}> (<t:${joinedAt}:R>)` },
                 { name: '`📆` Stworzył konto', value: `<t:${createdAt}> (<t:${createdAt}:R>)` },
                 { name: `\`🎭\` Role (${targetMember.roles.cache.size - 1})`, value: roles },
-                { name: '`❓` Inne', value: `**• Bot:** ${isBot}\n**• ID:** ${targetMember.user.id}` }
+                { name: '`❓` Inne', value: `**• Bot:** ${isBot}\n**• Zaproszeń:** ${inviteCount}\n**• ID:** ${targetMember.user.id}` }
             ]
         });
 
